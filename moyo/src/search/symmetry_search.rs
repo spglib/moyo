4f2e79163c79aa20aab2e0ee4f63b4f4c0baaea2
lan496 <kshinohara0508@gmail.com>
2024-12-29 13:48:50 +0900

    Set up python in CI

--- moyo/src/search/symmetry_search.rs
+++ moyo/src/search/symmetry_search.rs
@@ -1,7 +1,8 @@
 use super::primitive_cell::{PrimitiveCell, PrimitiveMagneticCell};
 use super::primitive_symmetry_search::{PrimitiveMagneticSymmetrySearch, PrimitiveSymmetrySearch};
 use crate::base::{
-    AngleTolerance, Cell, MagneticCell, MagneticMoment, MoyoError, ToleranceHandler,
+    AngleTolerance, Cell, MagneticCell, MagneticMoment, MagneticSymmetryTolerances, MoyoError,
+    RotationMagneticMomentAction, SymmetryTolerances, ToleranceHandler,
 };
 
 use log::debug;
@@ -14,26 +15,28 @@ pub fn iterative_symmetry_search(
     symprec: f64,
     angle_tolerance: AngleTolerance,
 ) -> Result<(PrimitiveCell, PrimitiveSymmetrySearch, f64, AngleTolerance), MoyoError> {
-    let mut current_symprec = symprec;
-    let mut current_angle_tolerance = angle_tolerance;
+    let mut tolerances = SymmetryTolerances {
+        symprec,
+        angle_tolerance,
+    };
 
     for _ in 0..MAX_TOLERANCE_HANDLER_TRIALS {
-        let mut tolerance_handler = ToleranceHandler::new(current_symprec, current_angle_tolerance);
+        let mut tolerance_handler = ToleranceHandler::new(tolerances);
 
         for _ in 0..MAX_SYMMETRY_SEARCH_TRIALS {
-            match PrimitiveCell::new(cell, tolerance_handler.symprec) {
+            match PrimitiveCell::new(cell, tolerance_handler.tolerances.symprec) {
                 Ok(prim_cell) => {
                     match PrimitiveSymmetrySearch::new(
                         &prim_cell.cell,
-                        tolerance_handler.symprec,
-                        tolerance_handler.angle_tolerance,
+                        tolerance_handler.tolerances.symprec,
+                        tolerance_handler.tolerances.angle_tolerance,
                     ) {
                         Ok(symmetry_search) => {
                             return Ok((
                                 prim_cell,
                                 symmetry_search,
-                                tolerance_handler.symprec,
-                                tolerance_handler.angle_tolerance,
+                                tolerance_handler.tolerances.symprec,
+                                tolerance_handler.tolerances.angle_tolerance,
                             ));
                         }
                         Err(err) => tolerance_handler.update(err),
@@ -44,22 +47,19 @@ pub fn iterative_symmetry_search(
         }
 
         // When the maximum number of symmetry search trials is reached, restart ToleranceHandler to try larger strides
-        current_symprec = tolerance_handler.symprec;
-        current_angle_tolerance = tolerance_handler.angle_tolerance;
-        debug!(
-            "Restart ToleranceHandler with symprec={}, angle_tolerance={:?}",
-            current_symprec, current_angle_tolerance
-        );
+        tolerances = tolerance_handler.tolerances.clone();
+        debug!("Restart ToleranceHandler with {:?}", tolerances);
     }
     debug!("Reach the maximum number of symmetry search trials");
     Err(MoyoError::PrimitiveSymmetrySearchError)
 }
 
-pub fn iterative_magnetic_symmetry_search<M: MagneticMoment>(
-    cell: &MagneticCell<M>,
+pub fn iterative_magnetic_symmetry_search<M: MagneticMoment + Clone>(
+    magnetic_cell: &MagneticCell<M>,
     symprec: f64,
     angle_tolerance: AngleTolerance,
     mag_symprec: Option<f64>,
+    action: RotationMagneticMomentAction,
 ) -> Result<
     (
         PrimitiveMagneticCell<M>,
@@ -70,9 +70,49 @@ pub fn iterative_magnetic_symmetry_search<M: MagneticMoment>(
     ),
     MoyoError,
 > {
-    let mut current_symprec = symprec;
-    let mut current_angle_tolerance = angle_tolerance;
-    let mut current_mag_symprec = mag_symprec.unwrap_or(symprec);
+    let mut tolerances = MagneticSymmetryTolerances {
+        symprec,
+        angle_tolerance,
+        mag_symprec: mag_symprec.unwrap_or(symprec),
+    };
 
-    todo!()
+    for _ in 0..MAX_TOLERANCE_HANDLER_TRIALS {
+        let mut tolerance_handler = ToleranceHandler::new(tolerances);
+
+        for _ in 0..MAX_SYMMETRY_SEARCH_TRIALS {
+            match PrimitiveMagneticCell::new(
+                &magnetic_cell,
+                tolerance_handler.tolerances.symprec,
+                tolerance_handler.tolerances.mag_symprec,
+            ) {
+                Ok(prim_mag_cell) => {
+                    match PrimitiveMagneticSymmetrySearch::new(
+                        &prim_mag_cell.magnetic_cell,
+                        tolerance_handler.tolerances.symprec,
+                        tolerance_handler.tolerances.angle_tolerance,
+                        tolerance_handler.tolerances.mag_symprec,
+                        action,
+                    ) {
+                        Ok(magnetic_symmetry_search) => {
+                            return Ok((
+                                prim_mag_cell,
+                                magnetic_symmetry_search,
+                                tolerance_handler.tolerances.symprec,
+                                tolerance_handler.tolerances.angle_tolerance,
+                                tolerance_handler.tolerances.mag_symprec,
+                            ));
+                        }
+                        Err(err) => tolerance_handler.update(err),
+                    }
+                }
+                Err(err) => tolerance_handler.update(err),
+            }
+        }
+
+        // When the maximum number of symmetry search trials is reached, restart ToleranceHandler to try larger strides
+        tolerances = tolerance_handler.tolerances.clone();
+        debug!("Restart ToleranceHandler with {:?}", tolerances);
+    }
+    debug!("Reach the maximum number of symmetry search trials");
+    Err(MoyoError::PrimitiveMagneticSymmetrySearchError)
 }
